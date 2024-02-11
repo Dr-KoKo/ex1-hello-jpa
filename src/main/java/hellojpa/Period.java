c7f3c80668196a27bb19bc2d24315c1074d57068
Denny-Ko <donghar@naver.com>
2024-02-12 07:13:37 +0900

    값 타입의 비교

--- src/main/java/hellojpa/Period.java
+++ src/main/java/hellojpa/Period.java
@@ -3,6 +3,7 @@ package hellojpa;
 import jakarta.persistence.Embeddable;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Embeddable
 public class Period {
@@ -15,5 +16,18 @@ public class Period {
     public Period(LocalDate startDate, LocalDate endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Period period = (Period) o;
+        return Objects.equals(startDate, period.startDate) && Objects.equals(endDate, period.endDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startDate, endDate);
     }
 }
